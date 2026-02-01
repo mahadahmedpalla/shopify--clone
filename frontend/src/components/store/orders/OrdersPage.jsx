ef846f45fb456b35c8da4a989d5eb9358a1b2491
mahadahmedpalla <0476mahad@gmail.com>
2026-02-01 05:32:09 -0800

    dep fix -subb

--- frontend/src/components/store/orders/OrdersPage.jsx
+++ frontend/src/components/store/orders/OrdersPage.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { supabase } from '../../../lib/supabase';
-import {
 import { OrderDetailModal } from './OrderDetailModal';
 import {
     Search,
